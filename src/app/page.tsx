dd0bd85acdae9a927f72368be634ec0cecffd178
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2025-01-04 16:41:27 +0200

    🌐 feat: adjusting responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import ModeToggle from "@/components/ModeToggle";
+import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 
@@ -5,13 +7,18 @@ export default function Home() {
   return (
     <>
       <SignedOut>
-        <SignInButton mode="modal" >
-          <button className="bg-red-500 text-white">login</button>
+        <SignInButton mode="modal">
+          <Button>Sign in</Button>
         </SignInButton>
       </SignedOut>
       <SignedIn>
         <UserButton />
       </SignedIn>
+
+      <ModeToggle/>
+      <Button variant="default">Button</Button>
+      <Button variant="secondary">Button</Button>
+      <Button variant="outline">Button</Button>
     </>
   );
 }
