55361ae3b8d4afab26eadbd2be945b778c06860c
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2025-01-09 00:45:28 +0200

    🌐 feat: createUser component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,24 @@
+import CreateUser from "@/components/CreateUser";
 import ModeToggle from "@/components/ModeToggle";
 import { Button } from "@/components/ui/button";
 import { prisma } from "@/lib/prisma";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
 
 
-export default  async function Home() {
+export default async function Home() {
+
+  const user = await currentUser();
+
   return (
-    <div>
-      Content of home page
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className=" lg:col-span-6">
+        {user ? <CreateUser /> : null}
+      </div>
+
+      <div className="hidden lg-block lg:col-span-4 sticky">
+          WhoToFollow
+      </div>
     </div>
   );
 }
